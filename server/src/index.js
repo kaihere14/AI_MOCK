7634f4cf5a7bc1cf2cc7a4c0d0062727cba9355b
kaihere14 <yashpalsharma9950@gmail.com>
2025-11-06 01:43:30 +0530

    fix: Update CORS origin to include production URL

--- server/src/index.js
+++ server/src/index.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:5173"],
+    origin: ["http://localhost:3000", "http://localhost:5173","https://prepdash.vercel.app"],
   })
 );
 
